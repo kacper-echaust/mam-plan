9d4b3f2b6f87973c28e002c9692386159a02ce0b
kacper-echaust <k9echaust@gmail.com>
2025-09-04 19:18:23 +0200

    extract cards data

---
+++ src/data/cards.ts
@@ -0,0 +1,41 @@
+export const cards = [
+  {
+    title: "koncepcja i wizualizacja",
+    price: "100-150",
+    description: "Optymalny układ funkcjonalny oraz realistyczne wizualizacje.",
+    list: [
+      "Układ funkcjonalny - ergonomia i plan rozmieszczenia mebli",
+      "Wizualizacje 3D - realistyczny podgląd wnętrza",
+      "Panoramy 360 - wirtualny spacer po projekcie",
+    ],
+    benefits:
+      "Realistyczne przedstawienie projektu przed jego realizacją. Możliwość wprowadzenia zmian na wczesnym etapie. Jasność co do ostatecznego wyglądu wnętrza.",
+  },
+  {
+    title: "projekt techniczny",
+    price: "150-200",
+    description:
+      "Rozszerzenie pierwszego pakietu o szczegółową dokumentację techniczną oraz spis materiałów, co pozwala na precyzyjne wykonanie projektu.",
+    list: [
+      "Układ funkcjonalny, Wizualizacje 3D i Panoramy 360 stopni",
+      "Dokumentacja techniczna - plany instalacji, rysunki mebli.",
+      "Spis materiałów - wykazd produktów potrzebny do remontu",
+    ],
+    benefits:
+      "Pełna kontrola nad procesem realizacji.Minimalizacja ryzyka błędów wykonawczych.Jasność co do kosztów i harmonogramu prac.",
+  },
+  {
+    title: "realizacja i nadzór",
+    price: "250-350",
+    description:
+      "Najbardziej kompleksowy pakiet, obejmujący wszystkie elementy projektowania oraz nadzór autorski, który gwarantuje zgodność realizacji z projektem.",
+    list: [
+      "Pakiet 1 i 2",
+      "Nadzór autorski - kontrola postępów prac na budowie",
+      "Regularne konsultacje z wykonawcami",
+      "Odbiór końcowy",
+    ],
+    benefits:
+      "Pełna spójność projektu z realizacją.Profesjonalne wsparcie na każdym etapie budowy.Gwarancja jakości i zadowolenia z finalnego efektu.",
+  },
+];
